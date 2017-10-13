67dff1f296b9b249dff6ff14e581ca9bc021b875
Suellyn Specie <sspecie13@gmail.com>
2017-10-13 12:53:11 -0300

    create

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = EmberRouter.extend({
 Router.map(function() {
   this.route('friends', function() {
     this.route('view', {path: 'view/:id'});
+    this.route('create');
   });
 });
 
